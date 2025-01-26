6c4971251785fe41964b99c5f24df47be7b1eba4
Atunde Goodness <atundeg@gmail.com>
2025-01-26 21:04:35 +0000

    update routes

--- frontend/energy_monitor/src/App.jsx
+++ frontend/energy_monitor/src/App.jsx
@@ -8,8 +8,8 @@ import Sidebar from './components/Sidebar'
 // import Subheader from './components/Subheader'
 import Dashboard from './pages/Dashboard'
 import Cost from './pages/Cost'
-import ApplianceUsage from './pages/ApplianceUsage'
-// import UsageByRoom from './pages/UsageByRoom'
+// import ApplianceUsage from './pages/ApplianceUsage'
+import UsageByRoom from './pages/UsageByRoom'
 // import Emissions from './pages/Emissions'
 
 import { Route, Routes } from 'react-router-dom'
@@ -24,10 +24,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/cost" element={<Cost />} />
-          <Route path="/appliances" element={<ApplianceUsage />} />
+          {/* <Route path="/appliances" element={<ApplianceUsage />} /> */}
+          <Route path="/usage-by-rooms" element={<UsageByRoom />} />
           {/* 
            />
-          <Route path="/usage-by-room" element={<UsageByRoom />} />
           <Route path="/emissions" element={<Emissions />} /> */}
         </Routes>
       </main>
